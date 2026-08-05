37e42cb8754bc658459cf2980d31312170306645
Jamie McCrae <spam@helper3000.net>
2026-08-05 17:20:48 +0100

    mgmt: mcumgr: trasnport: lorawan: Add transport mgmt support
    Adds support for bridging using this transport
    
    Signed-off-by: Jamie McCrae <spam@helper3000.net>

--- subsys/mgmt/mcumgr/transport/src/smp_lorawan.c
+++ subsys/mgmt/mcumgr/transport/src/smp_lorawan.c
@@ -14,6 +14,10 @@
 #include <mgmt/mcumgr/transport/smp_internal.h>
 #include <mgmt/mcumgr/transport/smp_reassembly.h>
 
+#ifdef CONFIG_MCUMGR_GRP_TRANSPORT
+#include <zephyr/mgmt/mcumgr/grp/transport_mgmt/transport_mgmt.h>
+#endif
+
 LOG_MODULE_REGISTER(smp_lorawan, CONFIG_MCUMGR_TRANSPORT_LORAWAN_LOG_LEVEL);
 
 static void smp_lorawan_downlink(uint8_t port, uint8_t flags, int16_t rssi, int8_t snr,
@@ -23,6 +27,21 @@ static int smp_lorawan_uplink(struct net_buf *nb);
 
 static uint16_t smp_lorawan_get_mtu(const struct net_buf *nb);
 
+#ifdef CONFIG_MCUMGR_GRP_TRANSPORT
+static bool smp_lorawan_bridge_connect(struct smp_transport_bridge *bridge, bool outgoing,
+				       uint32_t mode, bool same_transport,
+				       zcbor_state_t *input_data, zcbor_state_t *output_data);
+
+static void smp_lorawan_bridge_disconnect(struct smp_transport_bridge *bridge, bool outgoing);
+
+static int smp_lorawan_bridge_tx(const struct smp_transport_bridge *bridge, struct net_buf *nb,
+				 bool outgoing);
+
+static bool smp_lorawan_bridge_modes(zcbor_state_t *output_data, int *rc)
+
+static bool smp_lorawan_bridge_config_details(uint32_t mode, zcbor_state_t *output_data, int *rc);
+#endif
+
 static struct lorawan_downlink_cb lorawan_smp_downlink_cb = {
 	.port = CONFIG_MCUMGR_TRANSPORT_LORAWAN_FRAME_PORT,
 	.cb = smp_lorawan_downlink,
@@ -31,12 +50,24 @@ static struct lorawan_downlink_cb lorawan_smp_downlink_cb = {
 struct smp_transport smp_lorawan_transport = {
 	.functions.output = smp_lorawan_uplink,
 	.functions.get_mtu = smp_lorawan_get_mtu,
+#ifdef CONFIG_MCUMGR_GRP_TRANSPORT
+	.functions.bridge_connect = smp_lorawan_bridge_connect,
+	.functions.bridge_disconnect = smp_lorawan_bridge_disconnect,
+	.functions.bridge_output = smp_lorawan_bridge_tx,
+#if defined(CONFIG_MCUMGR_GRP_TRANSPORT_INFO_FUNCTIONS)
+	.functions.bridge_modes = smp_lorawan_bridge_modes,
+	.functions.bridge_config_details = smp_lorawan_bridge_config_details,
+#endif
+#endif
 };
 
-#ifdef CONFIG_SMP_CLIENT
+#if defined(CONFIG_SMP_CLIENT) || defined(CONFIG_MCUMGR_GRP_TRANSPORT)
 struct smp_client_transport_entry smp_lorawan_client_transport = {
 	.smpt = &smp_lorawan_transport,
 	.smpt_type = SMP_LORAWAN_TRANSPORT,
+#ifdef CONFIG_MCUMGR_GRP_TRANSPORT_INFO_FUNCTIONS
+	.name = "LoRaWAN",
+#endif
 };
 #endif
 
@@ -231,13 +262,93 @@ static uint16_t smp_lorawan_get_mtu(const struct net_buf *nb)
 	return (uint16_t)max_data_size;
 }
 
+#ifdef CONFIG_MCUMGR_GRP_TRANSPORT
+static bool smp_lorawan_bridge_connect(struct smp_transport_bridge *bridge, bool outgoing,
+				       uint32_t mode, bool same_transport,
+				       zcbor_state_t *input_data, zcbor_state_t *output_data)
+{
+	ARG_UNUSED(bridge);
+	ARG_UNUSED(outgoing);
+	ARG_UNUSED(input_data);
+	ARG_UNUSED(output_data);
+
+	if (mode != 0) {
+		smp_add_cmd_err(output_data, MGMT_GROUP_ID_TRANSPORT,
+				TRANSPORT_MGMT_ERR_INVALID_MODE);
+
+		return false;
+	}
+
+	if (same_transport == true) {
+		smp_add_cmd_err(output_data, MGMT_GROUP_ID_TRANSPORT,
+				TRANSPORT_MGMT_ERR_SAME_BRIDGE_DEVICE_DISALLOWED);
+		return false;
+	}
+
+	if (outgoing == true) {
+		smp_add_cmd_err(output_data, MGMT_GROUP_ID_TRANSPORT,
+				TRANSPORT_MGMT_ERR_TRASNPORT_OUTGOING_NOT_SUPPORTED);
+		return false;
+	}
+
+	return true;
+}
+
+static void smp_lorawan_bridge_disconnect(struct smp_transport_bridge *bridge, bool outgoing)
+{
+	ARG_UNUSED(bridge);
+	ARG_UNUSED(outgoing);
+}
+
+static int smp_lorawan_bridge_tx(const struct smp_transport_bridge *bridge, struct net_buf *nb,
+				 bool outgoing)
+{
+	ARG_UNUSED(bridge);
+	ARG_UNUSED(outgoing);
+
+	return smp_lorawan_uplink(nb);
+}
+
+#if defined(CONFIG_MCUMGR_GRP_TRANSPORT_INFO_FUNCTIONS)
+static bool smp_lorawan_bridge_modes(zcbor_state_t *output_data, int *rc)
+{
+	bool ok;
+
+	ok = zcbor_map_start_encode(output_data, 2) &&
+	     zcbor_tstr_put_lit(output_data, "id") &&
+	     zcbor_uint32_put(output_data, 0) &&
+	     zcbor_tstr_put_lit(output_data, "description") &&
+	     zcbor_tstr_put_lit(output_data, "LoRaWAN") &&
+	     zcbor_tstr_put_lit(output_data, "incoming") &&
+	     zcbor_bool_put(output_data, true) &&
+	     zcbor_map_end_encode(output_data, 2);
+
+	*rc = MGMT_RETURN_CHECK(ok);
+	return ok;
+}
+
+static bool smp_lorawan_bridge_config_details(uint32_t mode, zcbor_state_t *output_data, int *rc)
+{
+	if (mode == 0) {
+		return true;
+	}
+
+	smp_mgmt_reset_zse_writer(output_data);
+	smp_add_cmd_err(output_data, MGMT_GROUP_ID_TRANSPORT, TRANSPORT_MGMT_ERR_INVALID_MODE);
+	*rc = 0;
+
+	return false;
+}
+#endif
+#endif
+
 static void smp_lorawan_start(void)
 {
 	int rc;
 
 	rc = smp_transport_init(&smp_lorawan_transport);
 
-#ifdef CONFIG_SMP_CLIENT
+#if defined(CONFIG_SMP_CLIENT) || defined(CONFIG_MCUMGR_GRP_TRANSPORT)
 	if (rc == 0) {
 		smp_client_transport_register(&smp_lorawan_client_transport);
 	}
